0c654dac4f0100dd2b481bdeb4573f56d9e35b41
Mike Maliniak <Mike.maliniak@deltatre.com>
2019-12-05 17:45:48 +1100

    added user validation and request handling

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require
+const { check, validationResult } = require('express-validator/check');
 
 // @route GET api/users
 // @desc Register User
 // @access Public
-router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('user successfully added');
-});
+router.post(
+    '/',
+    [
+        check('name', 'Name is required').not().isEmpty(),
+        check('email', 'Please include vaild email address'),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        res.send('user successfully added');
+    }
+);
 
 module.exports = router;
