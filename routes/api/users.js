931a3702543b5edbc946d99245e31e8715dd0004
Mike Maliniak <Mike.maliniak@deltatre.com>
2019-12-06 14:31:08 +1100

    updated user model and user api route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User.model');
 
 // @route GET api/users
 // @desc Register User
@@ -12,13 +17,54 @@ router.post(
         check('email', 'Please include vaild email address'),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
 
         if(!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('user successfully added');
+
+        const { name, email, password } = req.body;
+
+        try {
+            // See if user exists
+            let user = await User.findOne({ email });
+            
+            if(user){
+                return res.status(400).json({ errors: [{ msg: 'User already exists'}]});
+            }
+
+            // Get users gravatar...based of their email
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            });
+
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+
+            // Encrypt the password using Bcrypt
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
+            const payload = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign
+        } catch(err) {
+            res.status(500).send('Server Error')
+        }
     }
 );
 
