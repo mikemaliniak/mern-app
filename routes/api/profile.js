7516f6876176fce96d846fac281845dfa0affc84
Mike Maliniak <Mike.maliniak@deltatre.com>
2019-12-12 17:35:36 +1100

    midway through education profile routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -149,12 +149,62 @@ router.delete('/', auth, async (req, res) => {
 // @access Private
 router.put('/experience', [auth, [
     check('title', 'Title is required').not().isEmpty(),
-    check('company', 'company is required').not().isEmpty(),
-    check('from', 'from is required').not().isEmpty()
-]], (req, res) => {
+    check('company', 'Company is required').not().isEmpty(),
+    check('from', 'From is required').not().isEmpty()
+]], async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array()});
+    }
+
+    // body data
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body;
+
+    const newExp = {
+        title, company, location, from, to, current, description
+    }
+
+    try {
+        const profile = await Profile.findOneAndUpdate({ user: req.user.id });
+
+        // Unshift is same as push but pushed to the front of the array
+        profile.experience.unshift(newExp);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+})
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc DELETE experience from profileb
+// @access Private
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        // Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch(err) {
+        console.log(err.message);
+        res.status(500).send('Server Error');
     }
 })
 
