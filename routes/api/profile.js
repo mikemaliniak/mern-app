bd89e93e1465a4900765d19fb5100dea4031df3b
Mike Maliniak <Mike.maliniak@deltatre.com>
2019-12-16 16:14:35 +1100

    added posts initial post route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 
@@ -273,4 +275,33 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
     }
 });
 
+// @route GET api/profile/github/:username
+// @desc GET user repos form github
+// @access Public
+
+router.get('/github/:username', (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubClientSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node.js' }
+        }
+
+        request(options, (error, response, body) => {
+            if(error) console.error(error);
+
+            if(response.statusCode !== 200) {
+                return res.status(404).json({
+                    msg: 'No Github profile found'
+                });
+            }
+
+            res.json(JSON.parse(body));
+        });
+
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 module.exports = router;
