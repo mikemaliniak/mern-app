a82752678b357c4e787966859cc8531fc05d977c
Mike Maliniak <Mike.maliniak@deltatre.com>
2019-12-23 16:35:31 +1100

    created CreateProfile component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
@@ -42,6 +43,7 @@ const App = () => {
                 <Route exact path='/register' component={ Register } />
                 <Route exact path='/login' component={ Login } />
                 <PrivateRoute exact path='/dashboard' component={ Dashboard } />
+                <PrivateRoute exact path='/create-profile' component={ CreateProfile } />
               </Switch>
             </section>
           </Fragment>
