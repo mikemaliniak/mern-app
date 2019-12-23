df62838ce6bcb283db7ab9b382d5451314a2bd35
Mike Maliniak <Mike.maliniak@deltatre.com>
2019-12-23 15:58:08 +1100

    added logic to handle user have and not have profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import React, { Fragment, useEffect } from 'react';
+import { Route, Redirect, Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getCurrentProfile } from '../../actions/profile';
 import Spinner from '../layout/Spinner';
 
-const Dashboard = ({ getCurrentProfile, auth, profile}) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading }}) => {
     useEffect(() => {
         // Invoke action similar to componentDidMount, but using hooks!
         getCurrentProfile();
     }, []);
-    return (
-        <div>
-            Dashboard
-            <Spinner />
-        </div>
-    )
+
+    return loading && profile === null ? 
+        <Spinner /> : 
+        <Fragment>
+            <h1 className="large text-primary">Dashboard</h1>
+            <p className="lead">
+                <i className="fas fa-user"></i>&nbsp;
+                Welcome { user && user.name }
+            </p>
+            {profile !== null ? (
+            <Fragment>has</Fragment>
+            ) : (
+            <Fragment>
+                <p>YOU HAVE NOT YET SET UP A PROFILE, PLEASE ADD SOME INFO</p>
+                <Link to='create-profile' className='btn btn-primary my-1'>
+                    Create profile
+                </Link>
+            </Fragment>)}
+        </Fragment>;
 }
 
 Dashboard.propTypes = {
