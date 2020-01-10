59c53319dcb9b6827b2920012b725ae2de9e6fcb
Mike Maliniak <Mike.maliniak@deltatre.com>
2020-01-10 11:42:01 +1100

    completed github element in profile

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -14,7 +14,29 @@ const ProfileGithub = ({ username, getGithubRepos, repos }) => {
             <h2 className="text-primary my-1">Github Repos</h2>
             {repos === null ? <Spinner /> : (
                 repos.map(repo => (
-                <div>{repo.full_name}</div>
+                <div key={repo.id} className="repo bg-white p-1 my-1">
+                    <div>
+                        <h4>
+                            <a href={repo.hml_url} target="blank" rel="noopener noreferrer">
+                                {repo.name}
+                            </a>
+                        </h4>
+                        <p>{repo.description}</p>
+                    </div>
+                    <div>
+                        <ul>
+                            <li className="badge badge-primary">
+                                Stars: {repo.stargazers_count}
+                            </li>
+                            <li className="badge badge-dark">
+                                Watchers: {repo.watchers_count}
+                            </li>
+                            <li className="badge badge-primary">
+                                Forks: {repo.forks_count}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
                 ))
             )}
         </div>
