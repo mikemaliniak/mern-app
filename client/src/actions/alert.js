5f8b04a30c264305dc288844c0f877fb88619474
Mike Maliniak <Mike.maliniak@deltatre.com>
2020-01-10 12:25:19 +1100

    tidied up alert actions

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -8,5 +8,7 @@ export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
         payload: { msg, alertType, id }
     });
     // Dispatch the remove alert function 5000 miliseconds after it has been added
-    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+    setTimeout(() => dispatch({ 
+        type: REMOVE_ALERT, 
+        payload: id }), timeout);
 }
