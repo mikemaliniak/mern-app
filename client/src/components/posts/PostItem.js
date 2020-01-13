b170791582875495b2b302f79aeecd08ad3574ac
Mike Maliniak <Mike.maliniak@deltatre.com>
2020-01-13 14:31:04 +1100

    coninuation of post component

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -5,7 +5,7 @@ import Moment from 'react-moment';
 import { connect } from 'react-redux';
 
 
-const PostItem = ({ auth, post: { id, text, name, avatar, user, likes, date }}) => {
+const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, date, comments }}) => {
     return (
         <div className="post bg-white p-1 my-1">
           <div>
@@ -23,8 +23,25 @@ const PostItem = ({ auth, post: { id, text, name, avatar, user, likes, date }}) 
                 {text}
             </p>
              <p className="post-date">
-                Posted on 04/16/2019
+                Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
             </p>
+            <button type="button" className="btn btn-light">
+                <i className="fas fa-thumbs-up" />
+                <span>{likes.length}</span>
+            </button>
+            <button type="button" className="btn btn-light">
+                <i className="fas fa-thumbs-down" />
+            </button>
+            <Link to={`/post/${_id}`} classname="btn btn-primary">
+                Discussion { comments.length > 0 && (
+                    <span className="">{comments.length}</span>
+                )}
+            </Link>
+            {!auth.loading && user === auth.user._id && (
+                <button className="btn btn-danger">
+                    <i className="fas fa-times" />
+                </button>
+            )}
           </div>
         </div>
     )
