787c1d2d1034598362947e026da3f87baf40ea1b
Mike Maliniak <Mike.maliniak@deltatre.com>
2020-01-21 11:19:29 +1100

    added clientside like actions and reducers to allowlikes to be added and removed

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,59 +3,67 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
+import { addLike, removeLike } from '../../actions/post';
 
 
-const PostItem = ({ auth, post: { _id, text, name, avatar, user, likes, date, comments }}) => {
-    return (
-        <div className="post bg-white p-1 my-1">
-          <div>
-            <a href="profile.html">
-              <img
-                className="round-img"
-                src={avatar}
-                alt=""
-              />
-              <h4>{name}</h4>
-            </a>
-          </div>
-          <div>
-            <p className="my-1">
-                {text}
-            </p>
-             <p className="post-date">
-                Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
-            </p>
-            <button type="button" className="btn btn-light">
-                <i className="fas fa-thumbs-up" />
-                { likes.length > 0 && (
-                    <span>{' '}{likes.length}</span>
-                )}
-            </button>
-            <button type="button" className="btn btn-light">
-                <i className="fas fa-thumbs-down" />
-            </button>
-            <Link to={`/post/${_id}`} className="btn btn-primary">
-                Discussion{' '} { comments.length > 0 && (
-                    <span className="comment-count">{comments.length}</span>
-                )}
-            </Link>
-            {!auth.loading && user === auth.user._id && (
-                <button className="btn btn-danger">
-                    <i className="fas fa-times" />
+const PostItem = ({ 
+    auth, 
+    post: { _id, text, name, avatar, user, likes, date, comments },
+    addLike,
+    removeLike
+    }) => {
+        return (
+            <div className="post bg-white p-1 my-1">
+            <div>
+                <a href="profile.html">
+                <img
+                    className="round-img"
+                    src={avatar}
+                    alt=""
+                />
+                <h4>{name}</h4>
+                </a>
+            </div>
+            <div>
+                <p className="my-1">
+                    {text}
+                </p>
+                <p className="post-date">
+                    Postedddd on <Moment format="YYYY/MM/DD">{date}</Moment>
+                </p>
+                <button type="button" className="btn btn-light" onClick={() => addLike(_id)}>
+                    <i className="fas fa-thumbs-up" />
+                    { likes.length > 0 && (
+                        <span>{' '}{likes.length}</span>
+                    )}
                 </button>
-            )}
-          </div>
-        </div>
-    )
+                <button type="button" className="btn btn-light" onClick={() => removeLike(_id)}>
+                    <i className="fas fa-thumbs-down" />
+                </button>
+                <Link to={`/post/${_id}`} className="btn btn-primary">
+                    Discussion{' '} { comments.length > 0 && (
+                        <span className="comment-count">{comments.length}</span>
+                    )}
+                </Link>
+                {!auth.loading && user === auth.user._id && (
+                    <button className="btn btn-danger">
+                        <i className="fas fa-times" />
+                    </button>
+                )}
+            </div>
+            </div>
+        )
 }
 
 PostItem.propTypes = {
     post: PropTypes.object.isRequired,
     auth: PropTypes.object.isRequired,
+    addLike: PropTypes.func.isRequired,
+    removeLike: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
     auth: state.auth
 });
 
-export default connect(mapStateToProps)(PostItem)
+export default connect(mapStateToProps, { addLike, removeLike })(PostItem)
