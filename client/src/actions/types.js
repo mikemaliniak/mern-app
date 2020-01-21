b8e53b5ea52d30864fb054c5fa9ce1183f4a0a01
Mike Maliniak <Mike.maliniak@deltatre.com>
2020-01-21 14:27:42 +1100

    started delete post action

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -18,3 +18,4 @@ export const REPO_NULL = 'REPO_NULL';
 export const GET_POSTS = 'GET_POSTS';
 export const POST_ERROR = 'POST_ERROR';
 export const UPDATE_LIKES = 'UPDATE_LIKES';
+export const DELETE_POST = 'DELETE_POST';
