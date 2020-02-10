c9092d91ab736eac26ddbd15a54437c0af14ec8b
Mike Maliniak <Mike.maliniak@deltatre.com>
2020-02-10 15:07:31 +1100

    added post component

---
+++ client/src/components/post/Post.js
@@ -0,0 +1,16 @@
+import React, {Fragment, useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+const Post = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+Post.propTypes = {
+
+}
+
+export default Post
