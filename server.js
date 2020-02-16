d0dc656103e0c2f77047d66087cd4c82c59065fe
Michael Maliniak <mike@maliniak.io>
2020-02-16 17:11:17 +1100

    simplifying response

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ if(process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.send('Hello world');
+        // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
