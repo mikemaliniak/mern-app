c32e725d4ad31e4b504d8033f8f33029723c4de5
Michael Maliniak <mike@maliniak.io>
2020-02-16 17:21:25 +1100

    converted all scripts to yarn

--- server.js
+++ server.js
@@ -19,11 +19,10 @@ app.use('/api/posts', require('./routes/api/posts'));
 // Serve static assets in production
 if(process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('./client/build'));
 
     app.get('*', (req, res) => {
-        res.send('Hello world');
-        // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
